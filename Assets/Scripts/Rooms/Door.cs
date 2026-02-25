7fd3a1cc428de7dae1cd4575b3588c281f26141a
Aleksey Khromov <khromofficial@icloud.com>
2026-02-26 01:25:57 +0300

    9th lesson traps

--- Assets/Scripts/Rooms/Door.cs
+++ Assets/Scripts/Rooms/Door.cs
@@ -11,9 +11,17 @@ public class Door : MonoBehaviour
         if (collision.tag == "Player")
         {
             if (collision.transform.position.x < transform.position.x)
+            {
                 cam.MoveCamera(nextRoom);
+                nextRoom.GetComponent<Room>().ActivateRoom(true);
+                previousRoom.GetComponent<Room>().ActivateRoom(false);
+            }
             else
+            {
                 cam.MoveCamera(previousRoom);
+                previousRoom.GetComponent<Room>().ActivateRoom(true);
+                nextRoom.GetComponent<Room>().ActivateRoom(false);
+            }
         }
     }
 }

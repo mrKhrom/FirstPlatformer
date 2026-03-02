e2767d41c158d548a72e6a702b5bdf8b764fdb09
Aleksey Khromov <khromofficial@icloud.com>
2026-03-02 20:16:24 +0300

    10.3 enemy death

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -34,7 +34,18 @@ public class Health : MonoBehaviour
             if (!dead)
             {
                 anim.SetTrigger("die");
-                GetComponent<PlayerMovement>().enabled = false; 
+
+                //Player
+                if (GetComponent<PlayerMovement>() != null)
+                    GetComponent<PlayerMovement>().enabled = false;
+
+                //Enemy
+                if (GetComponent<EnemyPatrol>() != null)
+                    GetComponent<EnemyPatrol>().enabled = false;
+                
+                if (GetComponent<MeleeEnemy>() != null)
+                    GetComponent<MeleeEnemy>().enabled = false;
+                
                 dead = true;
             }       
         }

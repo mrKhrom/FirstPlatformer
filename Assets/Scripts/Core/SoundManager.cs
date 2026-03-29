c31c9d8ef5016fc260d281eff9ebff41e13516be
Aleksey Khromov <khromofficial@icloud.com>
2026-03-29 15:03:44 +0300

    15th lesson Music options

--- Assets/Scripts/Core/SoundManager.cs
+++ Assets/Scripts/Core/SoundManager.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager instance { get; private set; }
-    private AudioSource source;
+    private AudioSource soundSource;
+    private AudioSource musicSource;
 
     private void Awake()
     {
         instance = this;
-        source = GetComponent<AudioSource>();
-        
+        soundSource = GetComponent<AudioSource>();
+        musicSource = transform.GetChild(0).GetComponent<AudioSource>();
+
         //Keep this object even when we load a new scene
         if (instance != null)
         {
@@ -17,12 +19,41 @@ public class SoundManager : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
         else if (instance != null && instance != this)
-        {
             Destroy(gameObject);
-        }
+        
+        //Asign initial volumes
+        ChangeSoundVolume(0);
+        ChangeMusicVolume(0);
     }
     public void PlaySound(AudioClip _sound)
     {
-        source.PlayOneShot(_sound);
+        soundSource.PlayOneShot(_sound);
+    }
+
+    public void ChangeSoundVolume(float _change)
+    {
+        ChangeSourceVolume(1, "soundVolume", _change, soundSource);
+    }
+
+    public void ChangeMusicVolume(float _change)
+    {
+        ChangeSourceVolume(0.3f, "musicVolume", _change, musicSource);
+    }
+
+    private void ChangeSourceVolume(float baseVolume, string volumeName, float _change, AudioSource source)
+    {
+        float currentVolume = PlayerPrefs.GetFloat(volumeName, 1);
+        currentVolume += _change;
+
+        if (currentVolume > 1)
+            currentVolume = 0;
+        else if (currentVolume < 0)
+            currentVolume = 1;
+
+        //Assign final volume
+        float finalVolume = currentVolume * baseVolume;
+        source.volume = finalVolume;
+
+        PlayerPrefs.SetFloat(volumeName, currentVolume);    
     }
 }
--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -3,14 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Game Over")]
     [SerializeField] private GameObject gameOverScreen;
     [SerializeField] private AudioClip gameOverSound;
+
+    [Header("Pause")]
+    [SerializeField] private GameObject pauseScreen;
 
     private void Awake()
     {
         gameOverScreen.SetActive(false);
+        pauseScreen.SetActive(false);
     }
-    
+    #region GAMEOVER
     public void GameOver()
     {
         gameOverScreen.SetActive(true);
@@ -30,6 +35,38 @@ public class UIManager : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+        #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false; //Stop play mode in editor
+        #endif
     }
+    #endregion
+    
+    #region Pause
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            if (pauseScreen.activeInHierarchy)
+                PauseGame(false);
+            else
+                PauseGame(true);
+    }
+    public void PauseGame (bool status)
+    {
+        pauseScreen.SetActive(status);
+
+        Time.timeScale = status ? 0 : 1; //If status is true, set time scale to 0, otherwise set it to 1
+    }
+
+    public void SoundVolume()
+    {
+        SoundManager.instance.ChangeSoundVolume(0.2f); //Increase sound volume by 0.1        
+    }
+
+    public void MusicVolume()
+    {
+        SoundManager.instance.ChangeMusicVolume(0.2f); //Increase music volume by 0.1        
+    }
+        
+    #endregion
 }
